f4b75e1f5516f6c8cb7aea4d11e1c9d13ba4e864
Inna Červenková <yes>
2023-09-01 01:07:13 +0200

    Minor adjustments in styles

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('funguju');
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollButton = document.querySelector('.scroll-top');
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+      scrollButton.style.display = 'block';
+    } else {
+      scrollButton.style.display = 'none';
+    }
+  });
+
+  scrollButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+});
